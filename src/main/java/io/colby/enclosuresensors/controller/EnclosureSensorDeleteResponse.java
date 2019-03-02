77d4767a8f5e299f96d4a18b5b264cccfc6ff3b2
Colby Leclerc <colby.leclerc@gmail.com>
2019-03-02 12:25:11 -0500

    - Updated verbage to reflect design doc - Removed message attribute

--- src/main/java/io/colby/enclosuresensors/controller/EnclosureSensorDeleteResponse.java
+++ src/main/java/io/colby/enclosuresensors/controller/EnclosureSensorDeleteResponse.java
@@ -1,40 +1,35 @@
 package io.colby.enclosuresensors.controller;
 
 /*
- * Copyright (c) 2019. CJ Software Company All rights reserved.
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
  * Written by Colby Leclerc <colby@colby.io>, January 1, 2018
  */
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class EnclosureSensorDeleteResponse {
 
-    private Integer id;
-    private String message;
+    private boolean deleted;
+    @JsonProperty("enclosure-id")
+    private Integer enclosureId;
 
-    public EnclosureSensorDeleteResponse(int id, boolean exist){
-        if (!exist){
-            this.message = "Resource not deleted. Enclosure ID does not exist";
-        } else {
-            this.message = "Resource deleted successfully";
-        }
-
-        this.id = id;
+    public EnclosureSensorDeleteResponse(int sensorId, boolean deleted){
+        this.deleted = deleted;
+        this.enclosureId = sensorId;
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getEnclosureId() {
+        return enclosureId;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setEnclosureId(Integer enclosureId) {
+        this.enclosureId = enclosureId;
     }
 
-    public String getMessage() {
-        return message;
+    public boolean isDeleted() {
+        return deleted;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
     }
 }

1eb4b86bd1d1e9570c82f00d25caf3d1c985bb04
Colby Leclerc <colby.leclerc@gmail.com>
2019-03-12 16:04:47 -0400

    - Added plant repository

--- src/main/java/io/colby/model/repository/PlantRepository.java
+++ src/main/java/io/colby/model/repository/PlantRepository.java
@@ -2,12 +2,16 @@ package io.colby.model.repository;
 
 import io.colby.model.entity.Plant;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface PlantRepository extends CrudRepository<Plant, Integer> {
 
 
     Plant findByPlantId(Integer plantId);
+
+    List<Plant> findAll();
+
+    List<Plant> findAllByEnclosureId(Integer enclosureId);
 
 }

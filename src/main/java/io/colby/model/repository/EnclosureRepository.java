06e3f6d2e54ac5b9df4177052984300ffbb44a05
Colby Leclerc <colby.leclerc@gmail.com>
2019-03-12 16:04:17 -0400

    - sensortype enum reflects design docs

--- src/main/java/io/colby/model/repository/EnclosureRepository.java
+++ src/main/java/io/colby/model/repository/EnclosureRepository.java
@@ -2,11 +2,9 @@ package io.colby.model.repository;
 
 import io.colby.model.entity.Enclosure;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EnclosureRepository extends CrudRepository<Enclosure, Integer> {
 
 

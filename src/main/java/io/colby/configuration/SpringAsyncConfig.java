929103c59e443518a4c0b70f19d64ec9224643c1
Colby Leclerc <colby.leclerc@gmail.com>
2019-03-12 16:29:26 -0400

    - Added async support for resource endpoints

---
+++ src/main/java/io/colby/configuration/SpringAsyncConfig.java
@@ -0,0 +1,30 @@
+package io.colby.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig implements AsyncConfigurer {
+
+    @Override
+    @Bean(name = "asyncExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AGMSpringAsyncThread-");
+        executor.initialize();
+        
+        return executor;
+    }
+
+
+
+}

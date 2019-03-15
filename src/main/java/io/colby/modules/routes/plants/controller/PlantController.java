81ff8e5bad9d93efbbac6cbf0ec24efdd4199513
Colby Leclerc <colby.leclerc@gmail.com>
2019-03-14 22:02:35 -0400

    - Added docs - Changed return type of findByPlantId to Optional

--- src/main/java/io/colby/modules/routes/plants/controller/PlantController.java
+++ src/main/java/io/colby/modules/routes/plants/controller/PlantController.java
@@ -5,6 +5,7 @@ import io.colby.modules.auth.MetaID;
 import io.colby.modules.auth.Token;
 import io.colby.modules.auth.model.AuthenticationModel;
 import io.colby.modules.auth.model.AuthorizationModel;
+import io.colby.modules.routes.enclosures.model.entity.Enclosure;
 import io.colby.modules.routes.plants.model.entity.Plant;
 import io.colby.modules.routes.plants.model.repository.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,17 +24,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 //TODO change authentication and authorization requests from direct model call to service call
 //TODO update documentation to reflect changes in response JSON not being a map, and removing plant sensors from response
 //TODO change endpoint from /sensor/enclosure to /enclosure
+//TODO add PUT
 @RestController
 public class PlantController {
-
-//    final
-//    private PlantRepository repository;
-
-
-//    @Autowired
-//    public PlantController(PlantRepository repository) {
-//        this.repository = repository;
-//    }
 
     @Autowired
     PlantRepository plantRepository;
@@ -42,7 +35,7 @@ public class PlantController {
             method = RequestMethod.GET)
     @ResponseBody
     @Async("asyncExecutor")
-    public CompletableFuture<Plant> getSingleEnclosure(
+    public CompletableFuture<Plant> getSinglePlant(
             @PathVariable("id") int id,
             @RequestHeader(value = "Authorization") String auth,
             HttpServletResponse response
@@ -62,35 +55,44 @@ public class PlantController {
             return CompletableFuture.completedFuture(null);
         }
 
-        return CompletableFuture.completedFuture(plantRepository.findByPlantId(id));
+        Optional<Plant> plantSearch = plantRepository.findByPlantId(id);
 
-    }
-
-    @RequestMapping(value = {"/plants"},
-            method = RequestMethod.GET)
-    @ResponseBody
-    @Async("asyncExecutor")
-    public CompletableFuture<List<Plant>> getAllPlants(
-            @RequestHeader(value = "Authorization") String auth,
-            HttpServletResponse response
-    ) {
-
-        @NotBlank(message = "Authorization header cannot be blank")
-        String tokenParam = auth;
-
-        Token token = new Token(tokenParam);
-        MetaID metaId = new AuthenticationModel().getFromToken(token);
-
-        if (!metaId.doesExist()) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        if (!plantSearch.isPresent()){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return CompletableFuture.completedFuture(null);
         }
 
-//        TODO in auth or meta repo, get list of all enclosures associated with token/meta id. Then iterate and find
-
-        return CompletableFuture.completedFuture(plantRepository.findAll());
+        return CompletableFuture.completedFuture(plantSearch.get());
 
     }
+
+    //TODO add back in once auth completed
+//    @RequestMapping(value = {"/plants"},
+//            method = RequestMethod.GET)
+//    @ResponseBody
+//    @Async("asyncExecutor")
+//    public CompletableFuture<List<Plant>> getAllPlants(
+//            @RequestHeader(value = "Authorization") String auth,
+//            HttpServletResponse response
+//    ) {
+//
+//        @NotBlank(message = "Authorization header cannot be blank")
+//        String tokenParam = auth;
+//
+//        Token token = new Token(tokenParam);
+//        MetaID metaId = new AuthenticationModel().getFromToken(token);
+//
+//        if (!metaId.doesExist()) {
+//            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//            return CompletableFuture.completedFuture(null);
+//        }
+//
+////        TODO in auth or meta repo, get list of all enclosures associated with token/meta id. Then iterate and find
+//
+//
+//        return CompletableFuture.completedFuture(plantRepository.findAll());
+//
+//    }
 
 
     @RequestMapping(value = {"/plants"},
@@ -116,12 +118,15 @@ public class PlantController {
             return CompletableFuture.completedFuture(null);
         }
 
-        int enclosureId = Optional.of(plantRepository.save(request)).orElse(new Plant(0)).getPlantId();
+        Plant plant = plantRepository.save(request);
+        Optional<Plant> plantSearch = plantRepository.findByPlantId(plant.getPlantId());
 
-        if (enclosureId > 0)
-            response.setStatus(HttpServletResponse.SC_CREATED);
+        if (!plantSearch.isPresent()){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return CompletableFuture.completedFuture(null);
+        }
 
-        return CompletableFuture.completedFuture(plantRepository.findByPlantId(enclosureId));
+        return CompletableFuture.completedFuture(plantSearch.get());
 
     }
 
--- src/main/java/io/colby/modules/routes/plants/model/repository/PlantRepository.java
+++ src/main/java/io/colby/modules/routes/plants/model/repository/PlantRepository.java
@@ -4,14 +4,27 @@ import io.colby.modules.routes.plants.model.entity.Plant;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlantRepository extends CrudRepository<Plant, Integer> {
 
 
-    Plant findByPlantId(Integer plantId);
+    /**
+     * Returns populated Plant object based on passed plantId if
+     * plantId exists.
+     *
+     * @param plantId - ID of the Plant object
+     * @return - Populated Plant if found
+     */
+    Optional<Plant> findByPlantId(Integer plantId);
 
-    List<Plant> findAll();
-
+    /**
+     * Returns all plants associated with the passed enclosureId. Returns
+     * an empty list if no plants are found.
+     *
+     * @param enclosureId - ID of the associated Enclosure object
+     * @return List of populated Plant objects (if association exists)
+     */
     List<Plant> findAllByEnclosureId(Integer enclosureId);
 
 }
